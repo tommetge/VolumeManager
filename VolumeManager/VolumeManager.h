1895176f73037be29f3afb665807ff031c67f31e
tom metge <tom@accident-prone.com>
2013-11-10 13:19:58 -0700

    Initial Commit

---
+++ VolumeManager/VolumeManager.h
@@ -0,0 +1,13 @@
+//
+//  VolumeManager.h
+//  VolumeManager
+//
+//  Created by Tom Metge on 11/10/13.
+//  Copyright (c) 2013 Flying Paper Software. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface VolumeManager : NSObject
+
+@end
---
+++ VolumeManager/VolumeManager.m
@@ -0,0 +1,13 @@
+//
+//  VolumeManager.m
+//  VolumeManager
+//
+//  Created by Tom Metge on 11/10/13.
+//  Copyright (c) 2013 Flying Paper Software. All rights reserved.
+//
+
+#import "VolumeManager.h"
+
+@implementation VolumeManager
+
+@end
